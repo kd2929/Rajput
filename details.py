7d262b799f66edca68a61c8ec74830e2d5a16b03
Lord <110597375+kd2929@users.noreply.github.com>
2023-10-11 01:49:35 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,3 @@
-api_id = 15052451
-api_hash = "dbf8fdfc66d7a1a9bf359c036409aa14"
-bot_token = "5372996820:AAENkhchMoQnN7Jt5grVXhnHje4Nd3AxFlU"
-
+api_id = 24250238
+api_hash = "cb3f118ce5553dc140127647edcf3720"
+bot_token = "5970237571:AAFfzAJuBgcAZM4myoHPw9YwYqW0ztvOvkE"
